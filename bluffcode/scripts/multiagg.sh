5118d5e23ec7a6c016a33b643a85d62dcbcacba6
Marc Lanctot <marc.lanctot@maastrichtuniversity.nl>
2014-01-29 18:36:18 +0100

    ...

--- bluffcode/scripts/multiagg.sh
+++ bluffcode/scripts/multiagg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-matches="30 50 100 200 300 400 500 750 1000"
+matches="30 50 100 200 500"
 f1="aggfile.mcts.txt"
 f2="aggfile.oos.txt"
 
@@ -9,13 +9,19 @@ echo "" > $f2
 
 for m in $matches 
 do
+  echo "" 
+  date
+  echo "Starting ISMCTS, matches = $m"
   ./sim scratch/iss.initial.dat scratch/iss.initial.dat 1 1 agg $m > agg-ismcts.${m}.txt
-  ./sim scratch/iss.initial.dat scratch/iss.initial.dat 2 2 agg $m > agg-oos.${m}.txt
 
   t1=`cat agg-ismcts.${m}.txt | grep "Exploitabilities"`
-  t2=`cat agg-oos.${m}.txt | grep "Exploitabilities"`
-
   echo "ismcts $m $t1" >> $f1
+
+  date
+  echo "Starting OOS, matches = $m"
+  ./sim scratch/iss.initial.dat scratch/iss.initial.dat 2 2 agg $m > agg-oos.${m}.txt
+
+  t2=`cat agg-oos.${m}.txt | grep "Exploitabilities"`
   echo "oos $m $t2" >> $f2
 done
 
